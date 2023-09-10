77b2288ecf1d885e1b543c60b70f0efbe4c4835d
fatima369jad <143130179+fatima369jad@users.noreply.github.com>
2023-09-10 20:28:35 +0200

    Add project files.

---
+++ githubDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace githubDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,Fatima!");
+        }
+    }
+}
